127d3737556a200b54344c0d847b38568d4dcc1a
drewrolla <andrewong007@gmail.com>
2022-08-14 21:40:00 -0400

    hopefully fixed display issues

--- main.js
+++ main.js
@@ -1,44 +1,43 @@
 const weather = {
-    "apiKey": "23e2d80a7eaf577a56cf99cbe281d33b",
-    fetchWeather: function (city) {
+    "apiKey": "23e2d80a7eaf577a56cf99cbe281d33b", // should prob put this in another folder, can't remmber how to do this at the moment
+    getWeather: function (city) {
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q="
              + city 
-             + "&units=imperial&appid=" 
+             + "&units=imperial&appid=" // don't need to calc change from K to F - can specify which units of measurement you want to use instead = https://openweathermap.org/current#data
              + this.apiKey
         ).then((response)=> response.json())
         .then((data) => this.displayWeather(data));
     },
     displayWeather: function(data) {
-        // watched a video tutorial on this,
-        // goes into the dictionary containing the weather for each city
-        // and pulls the following information to display them
+        // watched tutorial for this part
+        // goes into the dict containing weather info for city
+        // and pulls info to display
         const { name } = data;
-        const { icon, description }= data.weather[0];
+        const { description }= data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
+        // acutally displays info
         document.querySelector(".city").innerText = "Weather in " + name;
-        document.querySelector(".icon").src = 
-        "https://openweathermap.org/img/wn/0"+ icon +".png" // supposed to display the weather icon next to the weather description
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°F";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + " mph";
-        // if we are searching for a location's weather, it will remove the hidden visibility styling
+        // if searching for location's weather, removes hidden visibility styling
         document.querySelector(".weather").classList.remove("loading");
-        document.body.style.backgroundImage = "url(https://source.unsplash.com/1600x900/?" + name + ")"
+        document.body.style.backgroundImage = "url(https://source.unsplash.com/1920x1080/?" + name + ")"
     },
     search: function () {
-        this.fetchWeather(document.querySelector(".search-bar").value)
+        this.getWeather(document.querySelector(".search-bar").value)
     }
 };
 
-// will run the search when the search icon is clicked
+// will run search when search icon is clicked
 document.querySelector(".search button").addEventListener("click", function () {
     weather.search();
 });
 
-// will run the search when enter key is hit
+// will run search when enter key is hit
 document.querySelector(".search-bar").addEventListener("keyup", function (event) {
     if (event.key == "Enter") {
         weather.search();
@@ -46,4 +45,4 @@ document.querySelector(".search-bar").addEventListener("keyup", function (event)
 });
 
 // will show a default city when loading up the page
-weather.fetchWeather("Jersey City");
+weather.getWeather("New York City");
